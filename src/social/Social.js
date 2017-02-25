d612d2e645364927a06606b8c837ae1dd33405ff
Allan Watson <allanwatson@Allans-iMac.local>
2017-02-25 14:07:30 +0000

    show social colours

--- src/social/Social.js
+++ src/social/Social.js
@@ -7,16 +7,16 @@ import SocialItem from './social-item/social-item.model.js';
 class Social extends Component {
 
   social = [
-    new SocialItem("linkedin", "https://www.linkedin.com/profile/view?id=52957269"),
-    new SocialItem("github", "https://github.com/shadowbeam/"),
-    new SocialItem("facebook", "https://www.facebook.com/watson.allan"),
-    new SocialItem("twitter", "https://twitter.com/shadowbeam_"),
-    new SocialItem("nike", "https://secure-nikeplus.nike.com/plus/profile/shadowbeam/"),
-    new SocialItem("google-plus", "https://plus.google.com/u/0/110066024819374028528"),
-    new SocialItem("lastfm", "http://www.last.fm/user/shadowbeam"),
-    new SocialItem("spotify", "https://play.spotify.com/user/shadowbeam"),
-    new SocialItem("songkick", "https://www.songkick.com/users/shadowbeam"),
-    new SocialItem("instagram", "http://www.instagram.com/shadowbeam"),
+    new SocialItem("linkedin", "https://www.linkedin.com/profile/view?id=52957269", "#0077b5"),
+    new SocialItem("github", "https://github.com/shadowbeam/", "#4078c0"),
+    new SocialItem("facebook", "https://www.facebook.com/watson.allan", "#3b5998"),
+    new SocialItem("twitter", "https://twitter.com/shadowbeam_", "#1da1f2"),
+    new SocialItem("nike", "https://secure-nikeplus.nike.com/plus/profile/shadowbeam/", "#e2142d"),
+    new SocialItem("google-plus", "https://plus.google.com/u/0/110066024819374028528", "#dd4b39"),
+    new SocialItem("lastfm", "http://www.last.fm/user/shadowbeam", "#d51007"),
+    new SocialItem("spotify", "https://play.spotify.com/user/shadowbeam", "#2ebd59"),
+    new SocialItem("songkick", "https://www.songkick.com/users/shadowbeam", "#f80046"),
+    new SocialItem("instagram", "http://www.instagram.com/shadowbeam", "#405de6"),
 
    ];
 
--- src/social/social-item/social-item.model.js
+++ src/social/social-item/social-item.model.js
@@ -2,10 +2,12 @@ export default class SocialItem {
 
     icon: string;
     url: string;
+    hex: string;
 
-    constructor(icon: string, url:string) {
+    constructor(icon: string, url:string, hex:string) {
         this.icon = icon;
         this.url = url;
+        this.hex = hex;
     }
 
 }
