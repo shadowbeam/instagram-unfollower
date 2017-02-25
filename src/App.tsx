627ad0322cbdf4f315b2429776894bc8865a2ae9
Allan Watson <allanwatson@Allans-iMac.local>
2017-02-25 11:20:21 +0000

    removed social

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Header from './header/Header.js';
 import Social from './social/Social.js';
 import './App.less';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+
+
+  public render() {
+
     return (
       <div className='App'>
-
         <div className='bg right'></div>
         <div className='bg left'></div>
-
-       <Header></Header>
+        <Header></Header>
+        <Social></Social>
       </div>
     );
   }
+
 }
 
 export default App;
--- src/social/Social.js
+++ src/social/Social.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import './social.less';
-
-class Social extends React.Component {
-
-	public social = ['github', 'linkedin'];
-
-
-	  constructor(props : ITodoItemProps){
-    super(props);
-    this.state = { editText: this.props.todo.title };
-  }
-
-
-
-    return (
-        <div className='social'>
-		{social.map((number) =>
-        <SocialItem key={number.toString()} value={number} />
-      )}
-
-
-        </div>
-    );
-  }
-
-function SocialItem(props) {
-
-  return (
-	<div className='icon'>
-			<i className={`fa fa-${props.value}`} aria-hidden="true"></i>
-	</div>
-  );
-}
-
-export default Social;
+// import React, { Component } from 'react';
+// import './social.less';
+//
+// class Social extends React.Component {
+//
+// 	// const social = ['github', 'linkedin'];
+//
+//
+// 	  constructor(props : ITodoItemProps){
+//     super(props);
+//     this.state = { editText: this.props.todo.title };
+//   }
+//
+//
+//
+//     return (
+//         <div className='social'>
+// 		{social.map((number) =>
+//         <SocialItem key={number.toString()} value={number} />
+//       )}
+//
+//
+//         </div>
+//     );
+//   }
+//
+// function SocialItem(props) {
+//
+//   return (
+// 	<div className='icon'>
+// 			<i className={`fa fa-${props.value}`} aria-hidden="true"></i>
+// 	</div>
+//   );
+// }
+//
+// export default Social;
