660fb1c4d6378ddea0e8086f30872c51664171c9
Allan Watson <allan.watson91@gmail.com>
2017-09-02 18:14:29 +0100

    model for following

---
+++ src/model/following.ts
@@ -0,0 +1,13 @@
+export class Following {
+
+    id: string;
+    username: string;
+    full_name: string;
+
+    constructor(id: string, username: string, full_name: string) {
+        this.id = id;
+        this.username = username;
+        this.full_name = full_name;
+    }
+
+}
