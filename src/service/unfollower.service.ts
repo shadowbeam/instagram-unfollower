e5b7d981334dce1a49d13250f0e290242e44b816
Allan Watson <allan.watson91@gmail.com>
2017-09-02 22:37:16 +0100

    unfollower service

---
+++ src/service/unfollower.service.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import * as LineByLineReader from 'line-by-line';
+import {Following} from "../model/following";
+
+export class Unfollower {
+
+    rl;
+
+    unfollow(): void {
+
+        this.rl = new LineByLineReader('followers.txt');
+
+        this.rl.on('line', this.readNextLine);
+
+    }
+
+    readNextLine = (line: string): void => {
+
+        this.rl.pause();
+
+        try {
+            let follower = JSON.parse(line.replace(/,$/, ""));
+
+            console.log('Line from file:', follower.id);
+        } catch (e) {
+            console.error("Could not parse the following line: " + line);
+        }
+
+        setTimeout(this.wait, 23000); //wait 23 seconds
+
+    }
+
+    wait = (): void => {
+        this.rl.resume();
+    }
+}
